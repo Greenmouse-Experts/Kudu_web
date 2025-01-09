7fa7005e5c50b5e332939a585864cdc51b54436d
Anyanwu Chukwuemeka <anyanwue4@gmail.com>
2025-01-09 13:40:46 +0100

    refactor: conditionally render SideBar for improved layout on vendor layout

--- src/layouts/vendor/index.jsx
+++ src/layouts/vendor/index.jsx
@@ -15,7 +15,9 @@ const VendorLayout = () => {
         <>
             <div className="w-full flex flex-col md:flex-row h-full animate__animated animate__fadeIn bg-kuduLightGray">
                 <Header openMenu={toggleMenu} />
-                <SideBar />
+                <div className="hidden md:flex">
+                    <SideBar />
+                </div>
                 <div className="w-full lg:ml-[23%] md:mx-4 flex flex-col gap-5 md:ml-[23%] h-full">
                     <Outlet />
                 </div>
