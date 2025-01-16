dcfa36747c2fff6703d0d0a7cef2bb9eb6681fcd
Prof.T Tech <95409350+HamzatAbdulazeez@users.noreply.github.com>
2025-01-16 22:22:24 +0100

    Update

--- src/modules/Home/components/ShoppingExperience.jsx
+++ src/modules/Home/components/ShoppingExperience.jsx
@@ -1,12 +1,12 @@
 export default function ShoppingExperience() {
     return (
         <>
-            <div className="flex w-full flex-col md:gap-5 gap-2 py-5">
+            <div className="flex w-full flex-col md:gap-8 gap-5 py-5">
                 <div className="flex w-full flex-col gap-2 md:gap-4 justify-center items-center">
-                    <p className="md:text-2xl text-base font-semibold text-center flex">
+                    <p className="md:text-3xl text-base font-semibold text-center flex">
                         Enjoy A Smooth Shopping Experience
                     </p>
-                    <p className="text-sm text-center text-black flex">
+                    <p className="text-md text-center text-black flex">
                         Buy and sell with Kudu, where convenience meet quality
                     </p>
                 </div>
@@ -15,12 +15,12 @@ export default function ShoppingExperience() {
                         <img
                             src="https://res.cloudinary.com/do2kojulq/image/upload/v1735561313/kudu_mart/truck_3d_zqctuq.png"
                             alt="truck_3d"
-                            className="max-w-[70px] max-h-[70px] md:max-w-[100px] md:max-h-[100px] object-contain"
+                            className="max-w-[80px] max-h-[70px] md:max-w-[150px] md:max-h-[150px] object-contain"
                         />
-                        <p className="md:text-lg text-sm font-semibold text-center flex">
+                        <p className="lg:text-lg text-sm font-semibold text-center flex">
                             Safe Delivery
                         </p>
-                        <p className="text-center text-xs text-black flex">
+                        <p className="text-center text-md leading-loose text-black flex">
                             Get it faster with our Premium service of same day delivery at home
                         </p>
                     </div>
@@ -29,12 +29,12 @@ export default function ShoppingExperience() {
                         <img
                             src="https://res.cloudinary.com/do2kojulq/image/upload/v1735561313/kudu_mart/money_3d_lwqctf.png"
                             alt="truck_3d"
-                            className="max-w-[70px] max-h-[70px] md:max-w-[100px] md:max-h-[100px] object-contain"
+                            className="max-w-[70px] max-h-[70px] md:max-w-[150px] md:max-h-[150px] object-contain"
                         />
                         <p className="md:text-lg text-sm font-semibold text-center flex">
                             Swift Payment Methods
                         </p>
-                        <p className="text-center text-xs text-black flex">
+                        <p className="text-center text-md leading-loose text-black flex">
                             Get it faster with our Premium service of same day delivery at home
                         </p>
                     </div>
@@ -43,12 +43,12 @@ export default function ShoppingExperience() {
                         <img
                             src="https://res.cloudinary.com/do2kojulq/image/upload/v1735561313/kudu_mart/customer_support_vevdp4.png"
                             alt="truck_3d"
-                            className="max-w-[70px] max-h-[70px] md:max-w-[100px] md:max-h-[100px] object-contain"
+                            className="max-w-[70px] max-h-[70px] md:max-w-[150px] md:max-h-[150px] object-contain"
                         />
                         <p className="md:text-lg text-sm font-semibold text-center flex">
                             24/7 Customer Support
                         </p>
-                        <p className="text-center text-xs text-black flex">
+                        <p className="text-center text-md leading-loose text-black flex">
                             Get it faster with our Premium service of same day delivery at home
                         </p>
                     </div>
@@ -57,12 +57,12 @@ export default function ShoppingExperience() {
                         <img
                             src="https://res.cloudinary.com/do2kojulq/image/upload/v1735561313/kudu_mart/court_3d_xpdhmi.png"
                             alt="truck_3d"
-                            className="max-w-[70px] max-h-[70px] md:max-w-[100px] md:max-h-[100px] object-contain"
+                            className="max-w-[70px] max-h-[70px] md:max-w-[150px] md:max-h-[150px] object-contain"
                         />
                         <p className="md:text-lg text-sm font-semibold text-center flex">
                             Bidding Opportunity
                         </p>
-                        <p className="text-center text-xs text-black flex">
+                        <p className="text-center text-md leading-loose text-black flex">
                             Get it faster with our Premium service of same day delivery at home
                         </p>
                     </div>
