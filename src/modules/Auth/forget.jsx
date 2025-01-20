edffa08a7540e741b1b5f62255c73b202a595ced
Prof.T Tech <95409350+HamzatAbdulazeez@users.noreply.github.com>
2025-01-20 23:00:12 +0100

    Update

--- src/modules/Auth/forget.jsx
+++ src/modules/Auth/forget.jsx
@@ -81,7 +81,7 @@ function Forget() {
 
 
     return (
-        <div className={`w-full ${passwordBlock ? 'h-full' : 'h-screen'} flex flex-col justify-center items-center okayb`} style={{
+        <div className={`w-full ${passwordBlock ? 'h-full' : 'h-screen'} flex flex-col justify-center items-center`} style={{
             backgroundImage: `
   url(https://res.cloudinary.com/ddj0k8gdw/image/upload/v1736943190/Reset_Password_xqqki7.jpg
 `,
