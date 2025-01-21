75e22061268c04e7aba796a0fc51f077af451ed3
Prof.T Tech <95409350+HamzatAbdulazeez@users.noreply.github.com>
2025-01-21 23:53:52 +0100

    Update

--- src/modules/Auth/forget.jsx
+++ src/modules/Auth/forget.jsx
@@ -95,7 +95,7 @@ function Forget() {
             <div className="mb-6">
                 <Link to={'/'}>
                     <img
-                        src="https://res.cloudinary.com/do2kojulq/image/upload/v1735426588/kudu_mart/kudum1_nsw4jg.png"
+                        src="https://res.cloudinary.com/greenmouse-tech/image/upload/v1737499574/kuduMart/kudum_2-removebg-preview_kf5m0t.png"
                         alt="Kudu Logo"
                         className="h-12"
                     />
