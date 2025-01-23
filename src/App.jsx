bc0f06b8d838587e507fbbe19ff8deccebef09da
Anyanwu Chukwuemeka <anyanwue4@gmail.com>
2025-01-23 22:52:14 +0100

    Add JWT token validation and redirect logic; update layout and dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,27 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { routes } from "./routes";
 import { ModalProvider } from "./hooks/modal";
 import ReusableModal from "./components/ReusableModal";
+import { accessType, isTokenValid } from "./helpers/tokenValidator";
 
 function App() {
   const router = createBrowserRouter(routes);
+  const tokenValid = isTokenValid();
+  const userData = accessType();
+
+  if(tokenValid) {
+    if(userData.user.name === "Administrator") {
+      if(!window.location.href.includes('admin')) {
+        window.location.href = `/auth/admin/login`;
+      }
+    } else {
+      if(window.location.href.includes('admin')) {
+        window.history.back();
+      }
+    }
+  }
+
+  console.log(userData);
+  console.log(window.location.href.includes('admin'));
 
   return (
     <ModalProvider>
