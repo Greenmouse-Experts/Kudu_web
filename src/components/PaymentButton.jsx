511c484840efb2cd15e74ea2f5ae95beee01e9ac
Anyanwu Chukwuemeka <anyanwue4@gmail.com>
2025-02-12 01:26:50 +0100

    Update navigation paths and enhance user profile components; refactor subscription handling and improve loading states.

--- src/components/PaymentButton.jsx
+++ src/components/PaymentButton.jsx
@@ -3,28 +3,33 @@ import { Button } from "@material-tailwind/react";
 import { usePaystackPayment } from "react-paystack";
 import { toast } from "react-toastify";
 
-const PaymentButton = ({ config, onSuccess, user, onClose, children }) => {
+const PaymentButton = ({ config, onSuccess, user, onClose, children, noWidth }) => {
     console.log(user)
     // Initialize the payment function with the latest config.
     const initializePayment = usePaystackPayment(config);
 
     const handleClick = () => {
-        if (user.location) {
-            if (!config.publicKey) {
-                console.error("Payment key not loaded");
-                return;
+        if (user) {
+            if (user.location) {
+                if (!config.publicKey) {
+                    console.error("Payment key not loaded");
+                    return;
+                }
+                initializePayment({ onSuccess, onClose });
             }
-            initializePayment({ onSuccess, onClose });
+            else {
+                toast.error('Default Shipping Address not set, visit your profile to set up one')
+            }
         }
         else {
-            toast.error('Default Shipping Address not set, visit your profile to set up one')
+            initializePayment({ onSuccess, onClose });
         }
     };
 
     return (
         <Button
             onClick={handleClick}
-            className="w-3/4 py-3 px-4 flex justify-center gap-2 bg-kuduOrange text-white rounded-lg font-[500] transition-colors"
+            className={`${noWidth ? '' : 'w-3/4'} py-3 px-4 flex justify-center gap-2 bg-kuduOrange text-white rounded-lg font-[500] transition-colors`}
         >
             {children}
         </Button>
