ff5676ee1f4c3e92be66717c4bae75071b6ea48c
mosesinawo <inawo.moses@gmail.com>
2025-02-18 15:42:20 +0100

    update testimonials from admin completed

--- src/modules/SuperAdmin/Dashboard/inner-pages/faqs.jsx
+++ src/modules/SuperAdmin/Dashboard/inner-pages/faqs.jsx
@@ -8,58 +8,6 @@ import Faqs from "../../../../components/inner-pages/faqs/Faqs";
 const App = () => {
 
   const { data:faqs, isLoading, refetch } = useGetFaqs();
-  
-  // const fetchData = async () => {
-  //   try {
-  //     const productRequest = new Promise((resolve, reject) => {
-  //       mutate({
-  //         url: '/admin/products',
-  //         method: 'GET',
-  //         headers: true,
-  //         hideToast: true,
-  //         onSuccess: (response) => resolve(response.data.data),
-  //         onError: reject,
-  //       });
-  //     });
-
-  //     const categoryRequest = new Promise((resolve, reject) => {
-  //       mutate({
-  //         url: '/admin/categories',
-  //         method: 'GET',
-  //         headers: true,
-  //         hideToast: true,
-  //         onSuccess: (response) => resolve(response.data.data),
-  //         onError: reject,
-  //       });
-  //     });
-
-  //     const [productsData, categories] = await Promise.all([
-  //       productRequest,
-  //       categoryRequest,
-  //     ]);
-
-  //     // Merge categories with products
-  //     const mergedData = productsData.map((product) => {
-  //       const category = categories.find(
-  //         (cat) => cat.id === product.sub_category?.categoryId
-  //       );
-  //       return {
-  //         ...product,
-  //         category: category ? category.name : 'Unknown',
-  //       };
-  //     });
-
-  //     setProducts(mergedData);
-  //   } catch (error) {
-  //     console.error('Error fetching data:', error);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   fetchData();
-  // }, []); // Only run once on mount
 
   return (
     <div className="min-h-screen">
