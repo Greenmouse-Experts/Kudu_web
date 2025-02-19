7766586a687a4eec0fbf6fc44d008a4478b6f35c
mosesinawo <inawo.moses@gmail.com>
2025-02-19 23:12:57 +0100

    add view orders list

--- src/routes/userRoutes.jsx
+++ src/routes/userRoutes.jsx
@@ -11,6 +11,7 @@ import VendorAdverts from "../modules/User/modules/VendorAdverts";
 import Notification from "../modules/Notification/Notification";
 import PostNewAdvert from "../modules/User/modules/AddNewAdvert";
 import UpdateAdvert from "../modules/User/modules/UpdateAdvert";
+// import OrderDetails from "../modules/orders/OrderDetails";
 
 export const userRoutes = [
     {
@@ -28,6 +29,10 @@ export const userRoutes = [
                     {
                         path: 'orders',
                         element: <ProfileOrders />
+                    },
+                    {
+                        path: 'orders/order-details/:id',
+                        element: <OrderDetails />
                     },
                     {
                         path: 'view-orders',
