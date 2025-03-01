4c8043a162e0b1bebe3518adfce239477f9c2ecb
Anyanwu Chukwuemeka <anyanwue4@gmail.com>
2025-03-01 13:41:02 +0100

    Add ProductDescription component and remove ShippingEstimate component

--- src/modules/Auction/layouts/AuctionPage.jsx
+++ src/modules/Auction/layouts/AuctionPage.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import { geoLocatorProduct } from "../../../helpers/geoLocatorProduct";
 import { useLocation, useNavigate } from "react-router-dom";
+import useAppState from "../../../hooks/appState";
+import { useModal } from "../../../hooks/modal";
+import Modal from "../../../components/Modal";
 
 const AuctionPage = ({ auctions }) => {
   const [activeTab, setActiveTab] = useState("popular");
   const location = useLocation();
   const navigate = useNavigate();
+  const { user } = useAppState();
 
   const filteredAuctions = geoLocatorProduct(auctions);
+
+  const { openModal } = useModal();
 
   const capitalizeEachWord = (str) => {
     return str
@@ -17,6 +23,15 @@ const AuctionPage = ({ auctions }) => {
   };
 
   const handleNavigate = (auctionId) => {
+    if (!user) {
+      openModal({
+        size: "sm",
+        content: (
+          <Modal submitButton={false} text={'You must be logged in to view auction details. Please sign in or create an account to access this product.'} />
+        ),
+      });
+      return;
+    }
     const isAuctionPage = location.pathname.includes("/auction");
     const targetPath = isAuctionPage ? `product/${auctionId}` : `auction/product/${auctionId}`;
     navigate(targetPath);
@@ -33,12 +48,13 @@ const AuctionPage = ({ auctions }) => {
       </div>
 
       {/* Auction Listings */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
-        {activeTab === "popular" &&
-          filteredAuctions.map((auction) => {
-            const auctionLocation = auction.vendor?.location
-              ? auction.vendor.location
-              : null;
+      {filteredAuctions.length > 0 ?
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+          {
+            filteredAuctions.map((auction) => {
+              const auctionLocation = auction.vendor?.location
+                ? auction.vendor.location
+                : null;
 
               return (
                 <div
@@ -52,45 +68,45 @@ const AuctionPage = ({ auctions }) => {
                     className="w-full md:h-[300px] object-cover rounded"
                   />
 
-                {/* Condition Badge */}
-                <span className={`absolute top-2 right-2 ${auction.condition === "brand_new" ? "bg-[#34A853]" : "bg-[#FF0F00]"} text-white px-2 py-1 text-xs rounded`}>
-                  {capitalizeEachWord(auction.condition)}
-                </span>
-
-                {/* Auction Name */}
-                <h3 className="text-sm md:text-base font-semibold mt-3">
-                  {auction.name}
-                </h3>
-
-                {/* Lot Number */}
-                <p className="text-gray-500 mt-3 text-xs">
-                  Lot #{" "}
-                  <span className="text-[#FF6F22]">{auction.id}</span>
-                </p>
-
-                {/* Current Bid */}
-                <p className="text-base md:text-lg font-bold mt-3 text-green-600">
-                  <small className="text-black">Current Bid:</small>{" "}
-                  {auction.store.currency.symbol} {auction.price}
-                </p>
-
-                {/* Location */}
-                <p className="text-black text-xs mt-3">
-                  Location:{" "}
-                  <span className="font-semibold">
-                    {auctionLocation
-                      ? `${auctionLocation.city}, ${auctionLocation.state}, ${auctionLocation.country}`
-                      : "N/A"}
+                  {/* Condition Badge */}
+                  <span className={`absolute top-2 right-2 ${auction.condition === "brand_new" ? "bg-[#34A853]" : "bg-[#FF0F00]"} text-white px-2 py-1 text-xs rounded`}>
+                    {capitalizeEachWord(auction.condition)}
                   </span>
+
+                  {/* Auction Name */}
+                  <h3 className="text-sm md:text-base font-semibold mt-3">
+                    {auction.name}
+                  </h3>
+
+                  {/* Lot Number */}
+                  <p className="text-gray-500 mt-3 text-xs">
+                    Lot #{" "}
+                    <span className="text-[#FF6F22]">{auction.id}</span>
                   </p>
 
-                {/* View Details Button */}
-                <button
-                  onClick={() => handleNavigate(auction.id)}
-                  className="bg-[#FF6F22] text-white w-full py-3 mt-5 rounded-lg text-xs md:text-sm"
-                >
-                  View Details
-                </button>
+                  {/* Current Bid */}
+                  <p className="text-base md:text-lg font-bold mt-3 text-green-600">
+                    <small className="text-black">Current Bid:</small>{" "}
+                    {auction.store.currency.symbol} {auction.price}
+                  </p>
+
+                  {/* Location */}
+                  <p className="text-black text-xs mt-3">
+                    Location:{" "}
+                    <span className="font-semibold">
+                      {auctionLocation
+                        ? `${auctionLocation.city}, ${auctionLocation.state}, ${auctionLocation.country}`
+                        : "N/A"}
+                    </span>
+                  </p>
+
+                  {/* View Details Button */}
+                  <button
+                    onClick={() => handleNavigate(auction.id)}
+                    className="bg-[#FF6F22] text-white w-full py-3 mt-5 rounded-lg text-xs md:text-sm"
+                  >
+                    View Details
+                  </button>
 
                   {/* Monitor Button */}
                   <div className="flex text-left text-[#FF6F22] text-sm mt-5 cursor-pointer">
@@ -116,6 +132,7 @@ const AuctionPage = ({ auctions }) => {
             </div>
           </div>
         </div>
+      }
     </div>
   );
 };
