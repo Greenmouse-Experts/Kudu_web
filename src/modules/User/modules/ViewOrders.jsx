22d042b3b3a3a5dacc8c11fec6a62c2653f3b524
Anyanwu Chukwuemeka <anyanwue4@gmail.com>
2025-03-02 20:37:55 +0100

    Refactor ProfileOrders component to use a table for order display; implement bid submission form in BidInformation using react-hook-form for input handling and validation.

--- src/modules/User/modules/ViewOrders.jsx
+++ src/modules/User/modules/ViewOrders.jsx
@@ -1,81 +1,137 @@
 import React from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 import ReactStars from "react-rating-stars-component";
+import useApiMutation from "../../../api/hooks/useApiMutation";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Button } from "@material-tailwind/react";
+import Loader from "../../../components/Loader";
+import { dateFormat } from "../../../helpers/dateHelper";
 
 const OrderDetails = () => {
+      const {
+          register,
+          handleSubmit,
+          setValue,
+          getValues,
+          watch,
+          formState: { errors },
+      } = useForm();
+
+      const {id} = useParams();
+
+      const [rating, setRating] = useState(0);
+      const [orderDetails, setOrderDetails] = useState({});
+      const [disabled, setDisabled] = useState(false);
+      const [isLoading, setIsLoading] = useState(true);
+
+      const {mutate} = useApiMutation();
+
+
+
+      useEffect(() => {
+        getOrderDetails();
+      }, []);
+
+
+
+      const getOrderDetails = () => {
+          mutate({
+              url: `/user/order/items?orderId=${id}`,
+              method: "GET", 
+              headers: true,
+              hideToast: true,
+              onSuccess: (response) => {
+                  setOrderDetails(response.data.data);
+                  setIsLoading(false);
+              },
+              onError: (error) => {
+                  setIsLoading(false);
+              },
+          });
+      };
+
+
+
+      const ratingChanged = (newRating) => {
+          setRating(newRating);
+      };
+
+
+      const onSubmit = (data) => {
+        setDisabled(true);
+          mutate({
+              url: "/user/add/review",
+              method: "POST",
+              headers: true,
+              data: {
+                  orderId: id,
+                  productId: productDetails.id,
+                  rating: rating,
+                  comment: data.comment
+              },
+              onSuccess: (response) => {
+                setDisabled(false);
+              },
+              onError: () => {
+                setDisabled(false);
+              },
+          });
+      }
+
+  
+
+
+
+  if (isLoading) {
+    return (
+      <div className="w-full h-screen flex items-center justify-center">
+        <Loader />
+      </div>
+    );
+  }
+
+
+
+  const productDetails = JSON.parse(orderDetails[0].product);
+
+
   return (
     <div className=" w-full flex justify-between gap-6">
-      <div className="  p-6 bg-white shadow-md rounded-lg">
+      <div className="flex-1 p-6 bg-white shadow-md rounded-lg">
         <h2 className="text-xl font-semibold flex items-center gap-2">
-          <span>&larr;</span> Order Details
+          Order Details
         </h2>
 
-        <div className="mt-4 border-b pb-4">
-          <p className="text-gray-700">
-            <strong>Order n° 1187245272</strong>
-          </p>
-          <p className="text-gray-600">1 Item</p>
-          <p className="text-gray-600">Placed on 28-10-2024</p>
-          <p className="text-gray-900 font-semibold">Total: ₦19,406</p>
+        <div className="mt-4 border-b flex flex-col gap-2 pb-4">
+          <p className="text-gray-600">{orderDetails[0].quantity} Item</p>
+          <p className="text-gray-600"><span className="mr-1">&#128197;</span> Placed on {dateFormat(orderDetails[0].createdAt, 'dd-MM-yyy')}</p>
+          <p className="text-gray-900 font-semibold">Total: {productDetails.store.currency.symbol} {orderDetails[0].price}</p>
         </div>
 
         <div className="mt-4 p-4 border rounded-lg">
           <h3 className="text-lg font-semibold">ITEMS IN YOUR ORDER</h3>
-          <div className="flex items-center space-x-2 mt-2">
-            <span className="bg-green-500 text-white text-xs font-semibold px-2 py-1 rounded">
-              DELIVERED
-            </span>
-            <span className="bg-red-500 text-white text-xs font-semibold px-2 py-1 rounded">
-              NON-RETURNABLE
-            </span>
-          </div>
-          <p className="text-gray-700 mt-2 font-semibold">On 07-11</p>
 
           <div className="flex">
             <div className="flex items-center mt-4">
               <img
-                src="/path/to/boot-image.png"
+                src={productDetails.image_url}
                 alt="Boots"
                 className="w-16 h-16 object-cover"
               />
-              <div className="ml-4">
+              <div className="ml-4 flex flex-col gap-1">
                 <p className="text-gray-800 font-semibold">
-                  Steel Toe Waterproof Rubber Safety Rain Boots
+                 {productDetails.name}
                 </p>
-                <p className="text-gray-600">Size: EU 46</p>
-                <p className="text-gray-600">QTY: 1</p>
-                <p className="text-black font-semibold text-lg">₦18,506</p>
+                <p className="text-gray-600">QTY: {orderDetails[0].quantity}</p>
+                <p className="text-black font-semibold text-lg">{productDetails.store.currency.symbol} {productDetails.price}</p>
               </div>
             </div>
           </div>
 
-          <p className="text-sm text-gray-600 flex items-center mt-2">
-            <span className="mr-2">&#128197;</span> The return period ended on
-            (14-11-2024)
-            <a href="#" className="text-blue-600 ml-1">
-              Access our Return Policy.
-            </a>
-          </p>
         </div>
 
-        <div className="mt-4 flex justify-between">
-          <button className="bg-orange-500 text-white px-6 py-2 rounded-lg font-semibold">
-            Buy Again
-          </button>
-          <a href="#" className="text-orange-600 font-semibold">
-            See Status History
-          </a>
-        </div>
-
-        <div className="mt-6 grid grid-cols-2 gap-4 border-t pt-4">
-          <div>
-            <h3 className="text-lg font-semibold">PAYMENT INFORMATION</h3>
-            <p className="text-gray-700">Payment Method</p>
-          </div>
-          <div>
-            <h3 className="text-lg font-semibold">DELIVERY INFORMATION</h3>
-            <p className="text-gray-700">Delivery Method</p>
-          </div>
-        </div>
       </div>
       <div className="bg-white shadow-md rounded-lg p-6 h-fit flex-1">
 
@@ -95,7 +151,7 @@ const OrderDetails = () => {
 
           <p>Leave a rating</p>
           <ReactStars count={5} size={25} activeColor={"rgba(255, 111, 34, 1)"} onChange={ratingChanged} value={rating} />
-          <Button className="bg-kuduOrange text-white px-6 py-2 w-full mt-4 rounded-lg font-semibold">
+          <Button type="submit" className="bg-kuduOrange text-white px-6 py-2 w-full mt-4 rounded-lg font-semibold">
             Submit
           </Button>
 
