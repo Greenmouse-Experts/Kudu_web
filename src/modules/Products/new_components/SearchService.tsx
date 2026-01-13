62d3722cec2cf46a47d4864101f18a16132d6cdf
destiny <desto4q@gmail.com>
2026-01-13 16:36:41 +0100

    search

--- src/modules/Products/new_components/SearchService.tsx
+++ src/modules/Products/new_components/SearchService.tsx
@@ -43,6 +43,23 @@ export default function SearchService({ searchTerm }: { searchTerm: string }) {
 
   const items = query.data?.data || [];
 
+  if (items.length === 0) {
+    return (
+      <div className="empty-store mt-20">
+        <div className="text-center">
+          <img
+            src="https://res.cloudinary.com/ddj0k8gdw/image/upload/v1736780988/Shopping_bag-bro_1_vp1yri.png"
+            alt="Empty Store Illustration"
+            className="w-80 h-80 mx-auto"
+          />
+        </div>
+        <h1 className="text-center text-lg font-bold mb-4">
+          Search Service not found!
+        </h1>
+      </div>
+    );
+  }
+
   return (
     <div className="min-h-screen" data-theme="kudu">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
